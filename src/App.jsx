41e641ca91104b74e107ce9de7a9836c89c136d3
anna-olivia <a.wietholtz@posteo.de>
2024-11-07 10:59:25 +0100

    restore

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="container-fluid text-center">
+            <p> Ich bin ein footer &copy; </p>
+        </footer>
+    ) 
+}
+
+export default Footer;
